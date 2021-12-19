df73b8ed2bc14e3bf67d517590471ed99a03acab
Swaroop <swaroopkcs@gmail.com>
2021-12-18 20:18:37 -0500

    addition

---
+++ src/GitAddition.java
@@ -0,0 +1,13 @@
+
+public class GitAddition {
+
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=25;
+		int sum=num1+num2;
+		System.out.println("The sum of the numbers 10 and 25 is:   "+sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
