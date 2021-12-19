8a9738492d8791a17eb6518ea5fa6100714cf42e
Swaroop <swaroopkcs@gmail.com>
2021-12-18 20:21:58 -0500

    Check changes

--- src/GitAddition.java
+++ src/GitAddition.java
@@ -6,6 +6,8 @@ public class GitAddition {
 		int num2=25;
 		int sum=num1+num2;
 		System.out.println("The sum of the numbers 10 and 25 is:   "+sum);
+		System.out.println("");
+		System.out.println("Checking to see the status change");
 		// TODO Auto-generated method stub
 
 	}
